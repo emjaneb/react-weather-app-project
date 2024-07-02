d892a27f8311096f4112c874a40a793fb89b5025
emjaneb <137766632+emjaneb@users.noreply.github.com>
2024-07-02 14:10:19 +1000

    Created h1 & page footer HTML

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <div className="container">Weather App</div>
+      <div className="container">
+        <h1>Weather App</h1>
+
+        <footer>
+          This project was coded by <b>Emma Bartley</b> and is{" "}
+          <a
+            href="https://github.com/emjaneb/react-weather-app-project"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
